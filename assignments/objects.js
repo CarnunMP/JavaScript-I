19a8966d1dc3bbc851e59d1b5771b2b8ee419364
CarnunMP <carnun@hotmail.co.uk>
2019-08-19 12:39:34 +0100

    objects.js challenge 3 done

--- assignments/objects.js
+++ assignments/objects.js
@@ -66,10 +66,12 @@ console.log(intern5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+intern2.speak = function() {
+  console.log("Hello, my name is " + this.name + "!");
+}
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+intern5.multiplyNums = (num1, num2) => num1 * num2;
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
